36baac1b83dddbd570361021723e0bc01d80086b
Paola Carrero <paolacarrero731@gmail.com>
2022-06-27 22:12:14 -0500

    new exercies

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,22 +4,57 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args){
-        suma(10,20,30);
 
-        Coche autoMazda = new Coche();
-        autoMazda.incremento();
-        System.out.println(autoMazda.numPuertas);
-    }
+        int numeroif = 0;
+        int numeroWhile = 0;
+        int numeroFor = 0;
+        String estacion = "MARTES";
 
-    public static int suma(int a, int b, int c){
-        return a + b + c;
-    }
-}
+        /*condicionales*/
+        if (numeroif > 0){
+            System.out.println("Es positivo");
+        }else if(numeroif < 0){
+            System.out.println("Es negativo");
+        }
+        else if(numeroif == 0){
+            System.out.println("Es cero");
+        }
 
-class Coche{
-    public int numPuertas;
+        /*while*/
+        while(numeroWhile < 3){
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
 
-    public void incremento(){
-        this.numPuertas++;
+        /*do while*/
+        do{
+            System.out.println(numeroWhile);
+        }while(numeroWhile == 0);
+
+        /*for*/
+        for(int i = 0; i <= 3; i++){
+            System.out.println(i);
+        }
+
+        /*switch*/
+        switch(estacion){
+            case "VERANO":
+                System.out.println("nos encontramos en la estacion de VERANO");
+                break;
+            case "INVIERNO":
+                System.out.println("nos encontramos en la estacion de VERANO");
+                break;
+            case "OTOÑO":
+                System.out.println("nos encontramos en la estacion de VERANO");
+                break;
+            case "PRIMAVERA":
+                System.out.println("nos encontramos en la estacion de VERANO");
+                break;
+            default:
+                System.out.println("El valor ingresado no es una estacion");
+        }
+
+
+
     }
 }
